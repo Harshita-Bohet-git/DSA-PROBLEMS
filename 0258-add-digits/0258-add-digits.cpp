07160e220f6b619e67665cca1aa5bb98b6a23fd5
HARSHITA BOHET <harshitabohet@gmail.com>
2026-07-20 13:57:45 +0530

    Time: 0 ms (100%), Space: 8.7 MB (46.8%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num==0){
+            return num;
+        }
+        int res = num%9;
+        if(res==0){
+            return 9;
+        }
+        else{
+            return res;
+        }
+    }
+};
